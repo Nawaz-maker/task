a60da0b10d0751bde98c81a5a1f67c497cdf9014
Nawaz-maker <khan786786nawaz@gmail.com>
2025-07-04 10:27:31 +0530

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,11 @@
+# Basic mathematical operation for 2 numbers
+a,b=int(input('Enter a number: ')),int(input('Enter another number: '))
+print('Addition:      ',a+b)
+print('Subtraction:   ',a-b)
+print('Multiplication:',a*b)
+print('Division:      ',a/b)
+
+#personalised greeting
+a,b=(input('Enter your first name: ')),(input('Enter your last name: '))
+print('Hello '+a+' '+b+'! welcome to the python program.')
+
